52c9914de34cd96583a038084f8bdb630b90fa1c
daisuke <daisuke.satou2@e-harp.jp>
2015-09-01 13:58:35 +0900

    Add user microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,8 @@ describe User do
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:admin) }
+  it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -38,7 +40,7 @@ describe User do
   describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
-  end
+   end
 
    describe "when name is too long" do
     before { @user.name = "a" * 51 }
@@ -76,18 +78,18 @@ describe User do
     it { should_not be_valid }
   end
 
-   describe "when password is not present" do
-  before do
-    @user = User.new(name: "Example User", email: "user@example.com",
-                     password: " ", password_confirmation: " ")
+  describe "when password is not present" do
+   before do
+     @user = User.new(name: "Example User", email: "user@example.com",
+                      password: " ", password_confirmation: " ")
+   end
+   it { should_not be_valid }
   end
-  it { should_not be_valid }
-end
 
   describe "when password doesn't match confirmation" do
-  before { @user.password_confirmation = "mismatch" }
-  it { should_not be_valid }
-end
+   before { @user.password_confirmation = "mismatch" }
+   it { should_not be_valid }
+  end
 
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
@@ -122,5 +124,35 @@ end
  describe "remember token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
+  end
+
+  describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.to_a
+      @user.destroy
+      expect(microposts).not_to be_empty
+      microposts.each do |micropost|
+        expect(Micropost.where(id: micropost.id)).to be_empty
+      end
+    end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+    end
   end
 end
